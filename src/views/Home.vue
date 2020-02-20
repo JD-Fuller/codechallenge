979e46cdadb057023814fa521708b0633cf8806b
JD-Fuller <jdfuller@live.com>
2020-02-20 14:43:00 -0700

    added whiteboard items

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,9 @@
     <HelloWorld msg="Welcome to Your Vue.js App" />
 
     <h1>WhiteBoard Area</h1>
+    <h2>Key Terms</h2>
 
-    <h1>Mongoose</h1>
+    <h3>Mongoose</h3>
     <p>
       mongodb object modeling for node.js or JS Library that allows you the
       ability to define schemas with strongly typed data or High Level API's for
@@ -13,7 +14,10 @@
     </p>
     <br />
 
-    <h2>Dapper</h2>
+    <h3>Dapper</h3>
+    <p>Object mapper for .NET - Maps between Database and Language</p>
+
+    <h2></h2>
     <p></p>
   </div>
 </template>
