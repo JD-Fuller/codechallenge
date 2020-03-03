a083f408ace52ac4fe7b19403ef63870b401952c
JD Fuller <42724384+JD-Fuller@users.noreply.github.com>
2020-03-03 10:27:12 -0700

    pre-v-for work with titles

--- src/components/Whiteboard.vue
+++ src/components/Whiteboard.vue
@@ -1,5 +1,87 @@
 <template>
   <h1>WhiteBoard Area</h1>
+  <h2>Holding Place until database is established - we will utilize a v-for for this component<h2>
+  
+  <h3>TITLE</h3>
+  <p>
+  BODY
+  
+  </p>
+  <h4></h4>
+  <p>SOLUTION</p>
+  
+  
+    <h3>TITLE</h3>
+  <p>
+  BODY
+  
+  </p>
+  <h4></h4>
+  <p>SOLUTION</p>
+  
+  
+    <h3>TITLE</h3>
+  <p>
+  BODY
+  
+  </p>
+  <h4></h4>
+  <p>SOLUTION</p>
+    <h3>TITLE</h3>
+  <p>
+  BODY
+  
+  </p>
+  <h4></h4>
+  <p>SOLUTION</p>
+    <h3>TITLE</h3>
+  <p>
+  BODY
+  
+  </p>
+  <h4></h4>
+  <p>SOLUTION</p>
+    <h3>TITLE</h3>
+  <p>
+  BODY
+  
+  </p>
+  <h4></h4>
+  <p>SOLUTION</p>
+    <h3>TITLE</h3>
+  <p>
+  BODY
+  
+  </p>
+  <h4></h4>
+  <p>SOLUTION</p>
+    <h3>TITLE</h3>
+  <p>
+  BODY
+  
+  </p>
+  <h4></h4>
+  <p>SOLUTION</p>
+    <h3>TITLE</h3>
+  <p>
+  BODY
+  
+  </p>
+  <h4></h4>
+  <p>SOLUTION</p>  <h3>TITLE</h3>
+  <p>
+  BODY
+  
+  </p>
+  <h4></h4>
+  <p>SOLUTION</p>  <h3>TITLE</h3>
+  <p>
+  BODY
+  
+  </p>
+  <h4></h4>
+  <p>SOLUTION</p>
+  
 </template>
 
 <script>
